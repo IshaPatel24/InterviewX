f9fcca399e0ccbc5d4b6d6ef6247a02036af2b40
Isha Patel <ip150772@gmail.com>
2026-01-31 17:01:00 +0530

    Fix branding to InterviewX, resolve session creation bugs and user sync errors

--- backend/src/lib/inngest.js
+++ backend/src/lib/inngest.js
@@ -1,27 +1,28 @@
 import { Inngest } from "inngest";
 import { connectDB } from "./db.js";
 import User from "../models/User.js";
+import { upsetStreamUser, deleteStreamUser } from "./stream.js";
 
 export const inngest = new Inngest({ id: "InterviewX" });
 
 const syncUser = inngest.createFunction(
-    {id:"sync-user"},
-    {event:"clerk/user.created"},
-    async ({event}) => {
+    { id: "sync-user" },
+    { event: "clerk/user.created" },
+    async ({ event }) => {
         await connectDB()
 
-        const {id,email_addresses, first_name, last_name,image_url} = event.data;
-        
+        const { id, email_addresses, first_name, last_name, image_url } = event.data;
+
         const newUser = {
-            clerkId:id,
-            email:email_addresses[0]?.email_address,
-            name: '${first_name || ""} ${last_name || ""}',
-            profileImage:image_url 
+            clerkId: id,
+            email: email_addresses[0]?.email_address,
+            name: `${first_name || ""} ${last_name || ""}`,
+            profileImage: image_url
         };
 
         await User.create(newUser);
 
-        await UpsertStreamUser({
+        await upsetStreamUser({
             id: newUser.clerkId.toString(),
             name: newUser.name,
             image: newUser.profileImage,
